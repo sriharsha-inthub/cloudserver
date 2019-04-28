d9eb5022716f6ad4eecf3e594e8340d9f34edaf3
sriharsha-inthub <sriharsha@inthub.com.au>
2019-04-28 14:52:38 +0530

    Intial server setup shell script

--- ubuntu/initial-server-setup.sh
+++ ubuntu/initial-server-setup.sh
@@ -2,23 +2,26 @@
 
  echo "Initial server setup.."
  echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
- echo "[Step 1] - Logging in as Roo"
+ echo "[Task 1] - Logged in as Root with privatekey path"
  echo "ssh -i path/to/privatekey/ root@your_server_ip"
  echo ""
- echo "[Step 2] — Creating a New User"
- adduser sri --gecos "Sriharsha Vedurada,,," --disabled-password
+ echo "[Task 2] — Creating a New User"
+ adduser sri --gecos "Sriharsha Vedurada,,," --disabled-password > /dev/null 2>&1
  echo ""
- echo "[Step 3] — Granting Administrative Privileges"
- usermod -aG sudo sri
+ echo "[Task 3] — Granting Administrative Privileges"
+ usermod -aG sudo sri > /dev/null 2>&1
  echo ""
- echo "[Step 4] —Setting Up a Basic Firewall"
- ufw app list
- ufw allow OpenSSH
- ufw enable
+ echo "[Task 4] — Setting Up a Basic Firewall"
+ ufw app list > /dev/null 2>&1
+ echo "[Task 5] — Allow OpenSSH"
+ ufw allow OpenSSH > /dev/null 2>&1
+ echo "[Task 6] — Enable Firewall"
+ ufw enable -y > /dev/null 2>&1
+ echo "[Task 7] — Verifying Firewall is running"
  ufw status
  echo ""
- echo "Step 5 — Enabling External Access for Your Regular User"
- rsync --archive --chown=sri:sri ~/.ssh /home/sri
+ echo "[Task 6] — Enabling External Access for Your Regular User"
+ rsync --archive --chown=sri:sri ~/.ssh /home/sri > /dev/null 2>&1
  echo "################# Completed #################"
  
  
