1b8aae60742b033072f5698deb507b481f7504a7
Sriharsha <sriharsha@inthub.com.au>
2019-05-03 15:10:38 +0530

    Update with goadmin user details

--- ubuntu/gocdagent/initial-server-setup.sh
+++ ubuntu/gocdagent/initial-server-setup.sh
@@ -7,10 +7,10 @@
  echo "[Task 1] - Logged in as Root with privatekey path = ssh -i path/to/privatekey/ root@your_server_ip"
  echo -e "rootpwd\nrootpwd" | sudo passwd root > /dev/null 2>&1
  echo "[Task 2] — Creating a New User"
- adduser sri --gecos "Sriharsha Vedurada,,," --disabled-password > /dev/null 2>&1
+ adduser goadmin --gecos "goadmin,,," --disabled-password > /dev/null 2>&1
  echo "[Task 3] — Granting Administrative Privileges"
- usermod -aG sudo sri > /dev/null 2>&1
- echo -e "sripwd\nsripwd" | sudo passwd sri > /dev/null 2>&1
+ usermod -aG sudo goadmin > /dev/null 2>&1
+ echo -e "Goadmin@123\nGoadmin@123" | sudo passwd goadmin > /dev/null 2>&1
  echo "[Task 4] — Setting Up a Basic Firewall"
  ufw app list > /dev/null 2>&1
  echo "[Task 5] — Allow OpenSSH"
@@ -20,7 +20,7 @@
  echo "[Task 7] — Verifying Firewall is running"
  ufw status
  echo "[Task 6] — Enabling External Access for Your Regular User"
- rsync --archive --chown=sri:sri ~/.ssh /home/sri > /dev/null 2>&1
+ rsync --archive --chown=Goadmin@123:Goadmin@123 ~/.ssh /home/sri > /dev/null 2>&1
  echo "################# Completed #################"
  echo ""
  
