5ba2304c879edb2f4673edc6e8cfc78790141cb6
Sriharsha <sriharsha@inthub.com.au>
2019-05-07 11:50:41 +0530

    Updated firewall settings for ssh

--- ubuntu/gocdagent/initial-server-setup.sh
+++ ubuntu/gocdagent/initial-server-setup.sh
@@ -13,14 +13,19 @@
  echo -e "sripwd\nsripwd" | sudo passwd sri > /dev/null 2>&1
  echo "[Task 4] — Setting Up a Basic Firewall"
  ufw app list > /dev/null 2>&1
- echo "[Task 5] — Allow OpenSSH"
- ufw allow OpenSSH > /dev/null 2>&1
+ echo "[Task 5.1] — Disable complete SSH"
+ ufw deny 22
+ echo "[Task 5.2] — Allow SSH only from goserver"
+ #ufw allow OpenSSH > /dev/null 2>&1
+ ufw allow from 142.93.212.31 proto tcp to any port 22
+ ufw allow from 10.139.40.18 proto tcp to any port 22
  echo "[Task 6] — Enable Firewall"
  ufw --force enable > /dev/null 2>&1
  echo "[Task 7] — Verifying Firewall is running"
  ufw status
- echo "[Task 6] — Enabling External Access for Your Regular User"
- rsync --archive --chown=sri:sri ~/.ssh /home/sri > /dev/null 2>&1
+ 
+ #echo "[Task 6] — Enabling External Access for Your Regular User"
+ #rsync --archive --chown=sri:sri ~/.ssh /home/sri > /dev/null 2>&1
  echo "################# Completed #################"
  echo ""
  
