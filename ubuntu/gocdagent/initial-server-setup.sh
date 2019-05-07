219aee33f982ab4540d8acf402d231eb64484afa
Sriharsha <sriharsha@inthub.com.au>
2019-05-07 12:03:26 +0530

    Update initial-server-setup.sh

--- ubuntu/gocdagent/initial-server-setup.sh
+++ ubuntu/gocdagent/initial-server-setup.sh
@@ -13,14 +13,18 @@
  echo -e "sripwd\nsripwd" | sudo passwd sri > /dev/null 2>&1
  echo "[Task 4] — Setting Up a Basic Firewall"
  ufw app list > /dev/null 2>&1
- echo "[Task 5] — Allow OpenSSH"
- ufw allow OpenSSH > /dev/null 2>&1
- echo "[Task 6] — Enable Firewall"
+ echo "[Task 5] — Diasble OpenSSH"
+ ufw deny OpenSSH > /dev/null 2>&1
+ ufw deny 22 > /dev/null 2>&1
+ echo "[Task 6] — Allow OpenSSH only from goserver"
+ ufw allow from 142.93.212.31 proto tcp to any port 22 > /dev/null 2>&1
+ ufw allow from 10.139.40.18 proto tcp to any port 22 > /dev/null 2>&1
+ echo "[Task 7] — Enable Firewall"
  ufw --force enable > /dev/null 2>&1
- echo "[Task 7] — Verifying Firewall is running"
+ echo "[Task 8] — Verifying Firewall is running"
  ufw status
- echo "[Task 6] — Enabling External Access for Your Regular User"
- rsync --archive --chown=sri:sri ~/.ssh /home/sri > /dev/null 2>&1
+ #echo "[Task 6] — Enabling External Access for Your Regular User"
+ #rsync --archive --chown=sri:sri ~/.ssh /home/sri > /dev/null 2>&1
  echo "################# Completed #################"
  echo ""
  
